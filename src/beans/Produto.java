eb8e9bc44d891b24eb95706deea719fc99ac3345
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-18 19:39:08 -0300

    Criando a classe ExecutarProduto e importando a classe beans

--- src/beans/Produto.java
+++ src/beans/Produto.java
@@ -4,5 +4,4 @@ public class Produto {
 	public String descricao;
 	public String marca;
 	public float valor;
-	
 }
---
+++ src/implementacao/ExecutarProduto.java
@@ -0,0 +1,13 @@
+package implementacao;
+
+import beans.Produto;
+
+public class ExecutarProduto {
+
+	public static void main(String[] args) {
+		
+		Produto batom = new Produto();
+		
+	}
+
+}
