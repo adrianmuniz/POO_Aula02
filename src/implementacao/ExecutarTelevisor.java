aaa752bed94a18ee055d3dfdaae7473192964caa
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-18 21:11:13 -0300

    Criando e implementando a classe Tv

---
+++ src/implementacao/ExecutarTelevisor.java
@@ -0,0 +1,15 @@
+package implementacao;
+
+import beans.Televisor;
+
+public class ExecutarTelevisor {
+
+	public static void main(String[] args) {
+		Televisor tv = new Televisor();
+		
+		System.out.println(tv.mostrarStatus());
+
+		
+	}
+
+}
